8b9719b061c17b00542a12e616126adffea2528d
configtheworld <erkinture@gmail.com>
2022-05-01 00:17:22 +0300

    cra

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GiBlackHandShield } from 'react-icons/gi';
+import './App.css';
+
+function Header() {
+  return (
+    <div>
+      <GiBlackHandShield className="App-logo" alt="icon" />
+    </div>
+  );
+}
+
+export default Header;
