4bbd3f243237618a00432b734c0de5cadfed119e
configtheworld <erkinture@gmail.com>
2022-05-01 01:15:00 +0300

    ui creation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import {
+  AiOutlineHdd,
+  AiOutlineExperiment,
+  AiOutlineAlert,
+  AiOutlineEyeInvisible,
+  AiOutlineApi,
+} from 'react-icons/ai';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <div></div>
+        <h6>
+          <AiOutlineExperiment />
+          This url (<code>https://example.com/safe</code>) is <b>Safe</b>
+        </h6>
+        <div>
+          <h3>
+            DNS <AiOutlineHdd />
+          </h3>
+        </div>
+        <div>
+          <h3>
+            SSL
+            <AiOutlineApi />
+          </h3>
+        </div>
+        <div>
+          <h3>
+            Not Listed
+            <AiOutlineAlert />
+          </h3>
+        </div>
+        <div>
+          <h3>
+            No trackers <AiOutlineEyeInvisible />
+          </h3>
+        </div>
+
+        <div></div>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/configtheworld/security-extension"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { GiBlackHandShield } from 'react-icons/gi';
-import './App.css';
+import { RiLoaderLine, RiCursorFill } from 'react-icons/ri';
+import '../App.css';
 
 function Header() {
   return (
     <div>
-      <GiBlackHandShield className="App-logo" alt="icon" />
+      <RiLoaderLine className="App-icon-spinner" alt="icon" />
+      <RiCursorFill className="App-icon" />
+      <div className="HeaderBar">
+        <h4 className="HeaderTitle">Security Extension</h4>
+        <div className="HeaderDesc">
+          <i>Developed for Int. to Computer Security by Erkin</i>
+        </div>
+      </div>
     </div>
   );
 }
